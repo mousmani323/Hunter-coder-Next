19cdb49a056632ddf99ab801c49e51f4012dbfc6
Muhammad Omar <mousmani323@gmail.com>
2023-08-27 21:05:54 +0500

    2nd commit

--- app/blogpost/[slug]/page.js
+++ app/blogpost/[slug]/page.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-const Slug = ({ params }) => {
-  return (
+const Slug = async ({ params }) => {
+
+    const res = await fetch(`http://localhost:3000/api/getblogs?slug=${params.slug}`)
+    const data = await res.json();
+ 
+    return (
     <>
       <div>
-        <h2 className="m-10 text-4xl text-center">{params.slug} </h2>
-        <p  className="px-20" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quae illum fugit officia aspernatur omnis. Illo commodi illum consequatur possimus corrupti, natus blanditiis aut, est et quisquam eligendi. Nulla, dolore. Ratione aspernatur perferendis nesciunt consectetur vel voluptatibus omnis impedit nihil culpa ipsam suscipit tempore saepe dolorum dolore provident totam distinctio, ducimus a.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque non totam perferendis molestias consectetur aliquam in recusandae sit. Obcaecati fugiat temporibus quod ab tempora pariatur? Maxime amet corporis tempora ipsum!</p>
+        <h2 className="m-10 text-4xl text-center">{data && data.title} </h2>
+        <p  className="px-20" >{data && data.content}</p>
       </div>
     </>
   );
