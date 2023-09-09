cff285f72465baefdfce7da297b3f6a134d783b1
Muhammad Omar <mousmani323@gmail.com>
2023-09-09 18:27:31 +0500

    fav added

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Hunter Coder",
   description: "A blog for code hunters",
+  icons : {
+    icon: [
+      '/favicon.ico?v=4',
+    ],
+    shortcut: [
+      '/apple-touch-icon.png',
+    ],
+    apple: [
+      '/apple-touch-icon.png?v=4',
+    ]
+  },
+  manifest: '/site.webmanifest'
 };
 
 export default function RootLayout({ children }) {
